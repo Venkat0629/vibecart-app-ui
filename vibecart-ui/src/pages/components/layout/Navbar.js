5fca600293ec6d744d76419064881fabf74b075d
nparvathala <nparvathala@nisum.com>
2024-08-22 11:49:33 +0530

    header,footer,and homepage

--- vibecart-ui/src/pages/components/layout/Navbar.js
+++ vibecart-ui/src/pages/components/layout/Navbar.js
@@ -1,20 +1,64 @@
-import React from 'react'
+import React, { useCallback } from 'react';
+import { FaUser } from 'react-icons/fa';
+import { IoCartOutline } from 'react-icons/io5';
 import { useNavigate } from 'react-router-dom';
+<<<<<<< Updated upstream:vibecart-ui/src/pages/components/layout/Navbar.js
 
 import '../components.css'
 import { FaShoppingCart } from "react-icons/fa";
 
+=======
+import { useSearch } from '../Homepage/SearchContext';
+import debounce from 'lodash.debounce';
+import './Navbar.css';
+>>>>>>> Stashed changes:vibecart-ui/src/pages/components/Navbar.js
 
 const Navbar = () => {
+    const { handleSearch } = useSearch();
+    const navigate = useNavigate();
 
-  const navigate = useNavigate();
+    const debouncedHandleSearch = useCallback(
+        debounce((query) => handleSearch(query), 300),
+        [handleSearch]
+    );
 
-  const handleCartRoute =()=> {
-    navigate('/cart');
-  }
-  return (
-    <div className='navbar'><FaShoppingCart style={{cursor:"pointer"}} onClick={handleCartRoute}/></div>
-  )
-}
+    const handleSearchChange = (event) => {
+        const query = event.target.value;
+        if (event.key === 'Enter') {
+            handleSearch(query);
+        } else {
+            debouncedHandleSearch(query);
+        }
+    };
 
-export default Navbar
+    const handleCartRoute = () => {
+        navigate('/cart');
+    };
+
+    return (
+        <header className="navbar-container d-flex justify-content-between align-items-center p-3 shadow">
+            <div className="navbar-title">VibeCart</div>
+
+            <div className="navbar-search">
+                <input
+                    type="search"
+                    onKeyUp={handleSearchChange} // Listen for key press events
+                    onChange={handleSearchChange} // Handle changes in input value
+                    placeholder="Search"
+                    className="navbar-search-input"
+                />
+            </div>
+
+            <div className="navbar-icons d-flex align-items-center">
+                <div className="navbar-icon" onClick={handleCartRoute} aria-label="Cart">
+                    <IoCartOutline size={28} />
+                </div>
+                <div className="navbar-icon" aria-label="User Profile">
+                    <FaUser />
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
--- vibecart-ui/src/pages/routing/Routing.js
+++ vibecart-ui/src/pages/routing/Routing.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Route, Routes } from 'react-router-dom'
 import Homepage from '../components/Homepage'
 import Cart from '../components/cart/Cart'
@@ -5,24 +6,37 @@ import Checkout from '../components/cart/Checkout'
 import Navbar from '../components/layout/Navbar'
 import Footer from '../components/layout/Footer'
 import './routing.css';
+=======
+import { Route, Routes } from "react-router-dom";
+import Homepage from "../components/Homepage";
+import Cart from "../components/Cart";
+import Checkout from "../components/Checkout";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "./routing.css";
+import Home from "../Homepage/home";
+// import ProductPage from "../Homepage/ProductPage";
+// import CartPage from "../Homepage/CartPage";
+>>>>>>> Stashed changes
 
 export const Routing = () => {
-    return (
-        <div className='routing'>
-            <div className='navbarLayout'>
-                <Navbar />
-            </div>
-            <main className='content'>
-                <Routes>
-                    <Route path="/" element={<Homepage />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/checkout" element={<Checkout />} />
-                </Routes>
-            </main>
-            <div className='footerLayout'>
-                <Footer />
-            </div>
-
-        </div>
-    )
-}
+  return (
+    <div className="routing">
+      <div className="navbarLayout">
+        <Navbar />
+      </div>
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/product/:id" element={<ProductPage />} /> */}
+          {/* <Route path="/cartpage" element={<CartPage />} /> */}
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </main>
+      <div className="footerLayout">
+        <Footer />
+      </div>
+    </div>
+  );
+};
