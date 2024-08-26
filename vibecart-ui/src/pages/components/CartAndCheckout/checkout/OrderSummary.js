d1bb0b93caecfccb532bfd21e681b93df9568107
Vishnu Naga Miriyala <vmiriyala@nisum.com>
2024-08-26 19:20:29 +0530

    developed responsiveness

--- vibecart-ui/src/pages/components/CartAndCheckout/checkout/OrderSummary.js
+++ vibecart-ui/src/pages/components/CartAndCheckout/checkout/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CartProducts from '../cart/CartProducts'
 import './ordersummary.css'
+import ReusableButton from '../../../commoncomponents/ReusableButton'
 
 const OrderSummary = ({ cartData, cartBillData, navigateTo }) => {
   return (
@@ -25,6 +26,10 @@ const OrderSummary = ({ cartData, cartBillData, navigateTo }) => {
       <div className='ordersummary-bill-layout'>
         <p>Total</p>
        <p> <b>{cartBillData?.totalBill}</b></p>
+      </div>
+      <div className="place-order">
+        <p><strong>By placing an order, you are agreeing to our Privacy Policy and Terms of Use</strong></p>
+        <ReusableButton buttonName="Place Order with Cash" />
       </div>
     </div>
   )
