1ff947c811312cc063afef5789a7fc00089aa203
Vishnu Naga Miriyala <vmiriyala@nisum.com>
2024-08-26 14:31:57 +0530

    integrated checkout - 1

--- vibecart-ui/src/pages/components/CartAndCheckout/checkout/Checkout.js
+++ vibecart-ui/src/pages/components/CartAndCheckout/checkout/Checkout.js
@@ -1,21 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './checkout.css'
 import Shipping from './Shipping'
 import Payment from './Payment'
 import OrderSummary from './OrderSummary'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateAddressData, updatecartBillData, updateCartData } from '../../../redux-toolkit/CartSlice'
+import { calculateTotalBill } from '../../../commoncomponents/CommonFunctions'
+import { useNavigate } from 'react-router-dom'
 
 const Checkout = () => {
-  const { cartData,cartBillData:{totalBill},cartBillData } = useSelector((state) => state.cart);
+
+  const dispatch = useDispatch()
+  const { cartData, cartBillData: { totalBill }, cartBillData ,address} = useSelector((state) => state.cart);
+
+  const navigate = useNavigate();
+
+  const navigateTo = (path) => {
+    navigate(path);
+  }
+  const getCartData = () => {
+    const cartData = localStorage.getItem("cartData");
+    const shippingAddress = localStorage.getItem("shippingAddress");
+
+    return {
+      cartData: cartData?.length > 0 ? JSON.parse(cartData) : [],
+      address: shippingAddress ? JSON.parse(shippingAddress) : {}
+    }
+  }
+
+  useEffect(() => {
+    const {cartData, address} = getCartData();
+     dispatch(updateCartData(cartData));
+     dispatch(updateAddressData(address));
+    dispatch(updatecartBillData((calculateTotalBill(cartData))));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("cartBillData", JSON.stringify(cartBillData));
+  }, [totalBill]);
 
   return (
     <div class="checkout-container">
-        <div class="checkout-component-layout">
-            <div class="checkout-item "><Shipping></Shipping></div>
-            <div class="checkout-item ">Delivery & Gift Options</div>
-            <div class="checkout-item "><Payment/></div>
-        </div>
-        <div class="checkout-order-container"><OrderSummary cartData={cartData} cartBillData={cartBillData}/></div>
+      <div class="checkout-component-layout">
+        <div class="checkout-item "><Shipping address={address}/></div>
+        <div class="checkout-item ">Delivery & Gift Options</div>
+        <div class="checkout-item "><Payment address={address}/></div>
+      </div>
+      <div class="checkout-order-container"><OrderSummary cartData={cartData} cartBillData={cartBillData} navigateTo={navigateTo}/></div>
     </div>
   )
 }
--- vibecart-ui/src/pages/components/CartAndCheckout/checkout/OrderSummary.js
+++ vibecart-ui/src/pages/components/CartAndCheckout/checkout/OrderSummary.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import CartProducts from '../cart/CartProducts'
+import './ordersummary.css'
 
-const OrderSummary = ({cartData,cartBillData}) => {
+const OrderSummary = ({ cartData, cartBillData, navigateTo }) => {
   return (
     <div className='ordersummary-checkout-container'>
-           <h4>Order Summary</h4>
-           <p><b> - Your order ({cartData?.length} items)</b></p>
-           <CartProducts cartData={cartData}/>
+      <h4>Order Summary</h4>
+      <p><b>Your order ({cartData?.length} items)</b></p>
+      <CartProducts cartData={cartData} navigateTo={navigateTo} />
+      <hr></hr>
+      <div className='ordersummary-bill-layout'>
+        <p>Sub Total</p>
+        <p><b>{cartBillData?.totalBill}</b></p>
+      </div>
+      <div className='ordersummary-bill-layout'>
+        <p>Shipping Charges</p>
+        <p><b>0</b></p>
+      </div>
+      <div className='ordersummary-bill-layout'>
+        <p>Offers</p>
+        <p><b>NA</b></p>
+      </div>
+      <hr></hr>
+      <div className='ordersummary-bill-layout'>
+        <p>Total</p>
+       <p> <b>{cartBillData?.totalBill}</b></p>
+      </div>
     </div>
   )
 }
